e9828b91f1d38c634f9770ab4b9b300d14301825
Charles Zheng <charleszheng44@gmail.com>
2021-08-25 15:32:28 -0700

    add test case

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,8 +1,13 @@
-pub struct List<T> {
+use serde::{Deserialize, Serialize};
+use std::cmp::PartialEq;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct List<T: PartialEq> {
     head: Link<T>,
 }
 
-struct Node<T> {
+#[derive(Debug, Serialize, Deserialize)]
+struct Node<T: PartialEq> {
     ele: T,
     next: Link<T>,
 }
@@ -18,7 +23,35 @@ macro_rules! new_box_node {
     };
 }
 
-impl<T> List<T> {
+impl<'a, T: PartialEq> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref();
+            &node.ele
+        })
+    }
+}
+
+pub struct Iter<'a, T: PartialEq> {
+    next: Option<&'a Node<T>>,
+}
+
+pub struct IterMut<'a, T: PartialEq> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<'a, T: PartialEq> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.ele
+        })
+    }
+}
+
+impl<T: PartialEq> List<T> {
     pub fn new() -> List<T> {
         List { head: None }
     }
@@ -53,5 +86,43 @@ impl<T> List<T> {
                 Some(box_node) => cur = &mut box_node.next,
             }
         }
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
+    }
+}
+
+impl<T: PartialEq> PartialEq for List<T> {
+    fn eq(&self, other: &Self) -> bool {
+        !self.ne(other)
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        self.iter().zip(other.iter()).any(|(x, y)| x != y)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::List;
+    #[test]
+    fn serialization() {
+        let mut l1 = List::new();
+        l1.push_tail(1);
+        l1.push_tail(2);
+        l1.push_tail(3);
+        let jl = serde_json::to_string(&l1).unwrap();
+        println!("json format {}", jl);
+        let l2: List<i32> = serde_json::from_str(&jl).unwrap();
+        assert_eq!(l1, l2);
     }
 }
